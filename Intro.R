b1a6d55e0140108f64cda42fbd9e23311fd8cab3
Ramon <110137343+rayou2@users.noreply.github.com>
2022-10-28 12:53:33 -0400

    Add files via upload

--- Intro.R
+++ Intro.R
@@ -199,3 +199,77 @@ search()
 install.packages('tidyverse')
 
 # Part 2.3
+# Extract value 'Wt'
+metadata[1,1]
+# Extract value '1'
+metadata[1,3]
+# Extract third row
+metadata[3, ]
+# Extract third column 
+metadata[,3]
+# Extract third column as a data frame 
+metadata[ , 3, drop = FALSE]
+# Data frame containing first two columns
+metadata[ , 1:2]
+# Data frame containing first, third and sixth rows 
+metadata[c(1,3,6),]
+# Extract the celltype column for the first three samples 
+metadata[c('sample1','sample2','sample3'),'celltype']
+# To remind yourself of the column or row names:
+## Check column names or row names of the metadata data frame 
+colnames(metadata)
+rownames(metadata)
+# Extract the genotype column 
+metadata$genotype
+# Extract the first five values/elements of the genotype column 
+metadata$genotype[1:5]
+
+# exercises
+metadata[c('sample2','sample8'),c('genotype','replicate')]
+metadata$replicate[c(4,9)]
+metadata[,'replicate', drop = FALSE]
+
+metadata$celltype == 'typeA'
+logical_idx <- metadata$celltype == 'typeA'
+metadata[logical_idx,]
+
+which(metadata$celltype == 'typeA')
+which_idx <- which(metadata$celltype == 'typeA')
+metadata[which_idx,]
+
+which(metadata$replicate > 1)
+
+idx <- which(metadata$replicate > 1)
+metadata[idx,]
+## nesting in a single step 
+metadata[which(metadata$replicate > 1),]
+sub_meta <- metadata[which(metadata$replicate > 1),]
+
+# exercise 
+metadata[which(metadata$genotype == 'KO'),]
+
+# list
+list1[[2]]
+comp2 <- list1[[2]]
+class(comp2)
+
+list1[[1]]
+list1[[1]][1]
+
+# exercises
+random <- list(metadata,age,list1,samplegroup,number)
+random[[4]]
+
+#names
+names(list1)
+
+# name components of the list
+names(list1) <- c('species','df','number')
+names(list1)
+
+# Extract 'df' component 
+list1$df
+
+# exercise
+names(random) <- c('metadata','age','list1','samplegroup','number')
+random$age
